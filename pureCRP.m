ed8a4ed54576424214af41e2f311b6f4afc103c6
Staaar <star.s.fan@outlook.com>
2022-01-05 15:54:14 +0900

    Update pureCRP.m

--- pureCRP.m
+++ pureCRP.m
@@ -48,6 +48,10 @@ for ldx = 1:length(lambda)
             pkt_list(ptr:scs+blg,:) = sortrows(pkt_list(ptr:scs+blg,:),1);
         end
         idle_t = pkt_list(ptr,1) - min_t;
+        if idle_t < 0
+            disp FALSE_IDLE_MINUS
+            return
+        end
         min_t = pkt_list(ptr,1) + 1;
 
         % cycle period
@@ -93,13 +97,13 @@ for ldx = 1:length(lambda)
                 return
             end
             crp_min_t = 0;
-            crp_mu = 0.5 / 2;
+            crp_mu = 0.5 / 3;
             crp_ptr = 1;
             crp_blg = sum(crp_list(:,3)==1);
             crp_cnt = 0;
             crp_list(:,1) = crp_list(:,1) + exprnd(1/crp_mu,crp_blg,1);
             crp_list = sortrows(crp_list,1);
-            while sum(crp_list(:,3) == 1)  > 0
+            while sum(crp_list(:,3) == 1)  > 0 && crp_min_t < ENDTIME
                 if sum(crp_list(:,3) == 1) == 1
                     idx = find(crp_list(:,3) == 1);
                     if idx ~= crp_ptr
@@ -110,11 +114,7 @@ for ldx = 1:length(lambda)
                     dly = dly + crp_list(idx,1) - crp_list(idx,2) + 1;
                     blg = blg - 1;
                     crp_ptr = crp_ptr + 1;
-                    crp_blg = crp_blg - 1;
                     crp_min_t = crp_list(idx,1) + 1;
-                    if crp_blg ~= 0
-                        disp FALSE_CRP_BLG
-                    end
                 else
                     crp_min_t = crp_list(crp_ptr,1) + 1;
                     crp_sect = crp_list(crp_ptr:end,1) < crp_min_t;
@@ -123,10 +123,9 @@ for ldx = 1:length(lambda)
                         dly = dly + crp_list(crp_ptr,1) - crp_list(crp_ptr,2) + 1;
                         crp_list(crp_ptr,3) = -1;
                         crp_ptr = crp_ptr + 1;
-                        crp_blg = crp_blg - 1;
                         crp_cnt = crp_cnt + 1;
                         blg = blg - 1;
-                        if crp_cnt > 1
+                        if crp_cnt >= min(crp_blg - 1, 3)
                             break;
                         end
                     else
@@ -139,7 +138,7 @@ for ldx = 1:length(lambda)
                         end
                         crp_list(crp_ptr,1) = crp_list(crp_ptr,1) + 1 + exprnd(1/crp_mu);
                         crp_list(crp_ptr:end,:) = sortrows(crp_list(crp_ptr:end,:),1);
-                        crp_cnt = 0;
+                        % crp_cnt = 0;
                     end
                 end
             end
@@ -149,9 +148,11 @@ for ldx = 1:length(lambda)
             es_blg = max(1 + es_blg * exp(-mu * idle_t) + lambda_recur * coll_t,1);
             pkt_list(ptr:ptr+blg_end,:) = crp_list;
             ptr = scs + 1;
-            old_blg = pkt_list(ptr+blg_end+1:scs+blg,1) < min_t;
-            pkt_list(ptr+blg_end+1:scs+blg,1) = pkt_list(ptr+blg_end+1:scs+blg,1) + exprnd(1/mu,blg-blg_end-1,1) .* old_blg;
-            pkt_list(ptr:scs+blg,:) = sortrows(pkt_list(ptr:scs+blg,:),1);
+            old_blg = pkt_list(ptr:scs+blg,1) < min_t;
+            if sum(old_blg > 0)
+                pkt_list(ptr:scs+blg,1) = pkt_list(ptr:scs+blg,1) .* ~old_blg + (min_t + exprnd(1/mu,blg,1)) .* old_blg;
+                pkt_list(ptr:scs+blg,:) = sortrows(pkt_list(ptr:scs+blg,:),1);
+            end
             new_blg = sum(pkt_list(ptr+blg:end,1) < min_t);
             if new_blg > 0
                 pkt_list(ptr+blg:scs+blg+new_blg,1) = min_t + exprnd(1/mu,new_blg,1);
@@ -159,6 +160,15 @@ for ldx = 1:length(lambda)
                 blg = blg + new_blg;
             end
             pkt_list(ptr:scs+blg,:) = sortrows(pkt_list(ptr:scs+blg,:),1);
+            if sum(pkt_list(:,2) < min_t) - scs ~= blg
+                disp FALSE_BLG1
+                return
+            end
+        end
+        ac_blg = sum(pkt_list(:,2) < min_t) - scs;
+        if ac_blg ~= blg
+            disp FALSE_BLG
+            return
         end
         prev_end_t = min_t;
         mu = 0.5 / es_blg;
