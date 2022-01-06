c31cf3f14211e60c9afee3b2c5488d2754ead565
Staaar <star.s.fan@outlook.com>
2022-01-06 17:50:55 +0900

    Record CRP success mean

--- pureCRP.m
+++ pureCRP.m
@@ -1,24 +1,27 @@
 clear
 
 THEATA = 0.99;
-ENDTIME = 1e5;
+ENDTIME = 2e5;
 
-lambda = 0.01:0.01:0.4;
+lambda = 0.01:0.01:0.36;
 
 thrpt_list = zeros(length(lambda),1);
 dly_list = zeros(length(lambda),1);
+scs_crp_list = zeros(length(lambda),1); % Record the mean of success packets in CRP
 
-for ldx = 1:length(lambda)
-    tic
+tic
+parfor (ldx = 1:length(lambda),6)
     num = ceil(3 * lambda(ldx) * ENDTIME);
     ptr = 1;
     blg = 0;
     scs = 0;
     dly = 0;
     cnt = 0;
+    cnt_crp = 0;
     min_t = 0;
     es_blg = 10;
     ac_blg = 10;
+    scs_crp = 0;
     lambda_recur = 0;
     prev_end_t = 0;
     mu = 0.5 / es_blg;
@@ -50,7 +53,6 @@ for ldx = 1:length(lambda)
         idle_t = pkt_list(ptr,1) - min_t;
         if idle_t < 0
             disp FALSE_IDLE_MINUS
-            return
         end
         min_t = pkt_list(ptr,1) + 1;
 
@@ -86,18 +88,17 @@ for ldx = 1:length(lambda)
                     blg_end = sect - 1;
                 end
             end
-            coll_t = min_t - coll_start_t;
 
             % CRP period
+            cnt_crp = cnt_crp + 1;
             pkt_list(ptr:ptr+blg_end,1) = min_t;
             crp_start_t = min_t;
             crp_list = pkt_list(ptr:ptr+blg_end,:);
             if sum(crp_list(:,3) == 1) <= 1
                 disp FALSE_COLL
-                return
             end
             crp_min_t = 0;
-            crp_mu = 0.5 / 3;
+            crp_mu = 0.5 / 2;
             crp_ptr = 1;
             crp_blg = sum(crp_list(:,3)==1);
             crp_cnt = 0;
@@ -111,6 +112,7 @@ for ldx = 1:length(lambda)
                     end
                     crp_list(idx,3) = -1;   % column 3 == -1 => scs
                     scs = scs + 1;
+                    scs_crp = scs_crp + 1;
                     dly = dly + crp_list(idx,1) - crp_list(idx,2) + 1;
                     blg = blg - 1;
                     crp_ptr = crp_ptr + 1;
@@ -120,6 +122,7 @@ for ldx = 1:length(lambda)
                     crp_sect = crp_list(crp_ptr:end,1) < crp_min_t;
                     if sum(crp_sect) == 1
                         scs = scs + 1;
+                        scs_crp = scs_crp + 1;
                         dly = dly + crp_list(crp_ptr,1) - crp_list(crp_ptr,2) + 1;
                         crp_list(crp_ptr,3) = -1;
                         crp_ptr = crp_ptr + 1;
@@ -138,7 +141,7 @@ for ldx = 1:length(lambda)
                         end
                         crp_list(crp_ptr,1) = crp_list(crp_ptr,1) + 1 + exprnd(1/crp_mu);
                         crp_list(crp_ptr:end,:) = sortrows(crp_list(crp_ptr:end,:),1);
-                        % crp_cnt = 0;
+%                        crp_cnt = 0;
                     end
                 end
             end
@@ -148,6 +151,9 @@ for ldx = 1:length(lambda)
             es_blg = max(1 + es_blg * exp(-mu * idle_t) + lambda_recur * coll_t,1);
             pkt_list(ptr:ptr+blg_end,:) = crp_list;
             ptr = scs + 1;
+            if sum(pkt_list(1:scs,3) > 0) > 0
+                disp FALSE_CRP
+            end
             old_blg = pkt_list(ptr:scs+blg,1) < min_t;
             if sum(old_blg > 0)
                 pkt_list(ptr:scs+blg,1) = pkt_list(ptr:scs+blg,1) .* ~old_blg + (min_t + exprnd(1/mu,blg,1)) .* old_blg;
@@ -162,34 +168,36 @@ for ldx = 1:length(lambda)
             pkt_list(ptr:scs+blg,:) = sortrows(pkt_list(ptr:scs+blg,:),1);
             if sum(pkt_list(:,2) < min_t) - scs ~= blg
                 disp FALSE_BLG1
-                return
             end
         end
         ac_blg = sum(pkt_list(:,2) < min_t) - scs;
         if ac_blg ~= blg
             disp FALSE_BLG
-            return
         end
         prev_end_t = min_t;
         mu = 0.5 / es_blg;
     end
     thrpt_list(ldx) = scs / min_t;
     dly_list(ldx) = dly / scs;
-    toc
+    scs_crp_list(ldx) = scs_crp / cnt_crp;
 end
+toc
 
 figure
 plot(lambda,thrpt_list,'LineWidth',1)
-legend('Estimated','Location','northeast','Interpreter','latex','FontSize',14.4)
+legend('Estimated','Location','northwest','Interpreter','latex','FontSize',14.4)
 grid on
+xlim([0 0.26])
 xlabel('$\lambda$','Interpreter','latex','FontSize',17.6)
 ylabel('Throughput (packet/sec)','Interpreter','latex','FontSize',17.6)
 title('Pure ALOHA CRP','Interpreter','latex','FontSize',17.6)
 
 figure
 plot(lambda,dly_list,'LineWidth',1)
-legend('Estimated','Location','northeast','Interpreter','latex','FontSize',14.4)
+legend('Estimated','Location','northwest','Interpreter','latex','FontSize',14.4)
 grid on
+xlim([0 0.26])
+ylim([0 300])
 xlabel('$\lambda$','Interpreter','latex','FontSize',17.6)
 ylabel('Delay (sec)','Interpreter','latex','FontSize',17.6)
 title('Pure ALOHA CRP','Interpreter','latex','FontSize',17.6)
