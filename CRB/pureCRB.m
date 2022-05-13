983d26239e9138deff3cecbeb32eeca5e6556be6
Airloss <andrew.song.fan@outlook.com>
2022-05-13 12:29:44 +0900

    Update pureCRB.m

--- CRB/pureCRB.m
+++ CRB/pureCRB.m
@@ -13,12 +13,13 @@ tic
 for ldx = 1:length(lambda)
     scs = 0;
     dly = 0;
-    blg = 10;
-    mu = 1 / blg;
-    lambda_recur = 0.1;
+    mu = 10;
+    prob = 1 / mu;
+    lambda_recur = 0.5;
+    bof_list = zeros(N,1);
     arrival_list = poissrnd(lambda(ldx) / N,N,ENDTIME);
-    trans_list = zeros(N,3);    % transmit time | arrival time | synchronize state
-    queue = zeros(N,ceil(1.5 * lambda(ldx) * ENDTIME));
+    trans_list = zeros(N,4);    % transmit time | arrival time | packets number in queue | synchronize state
+    queue = zeros(N,ceil(1.5 * lambda(ldx) / N * ENDTIME));
 
     for tdx = 1:ENDTIME
         % Queue update before transmission
@@ -35,7 +36,37 @@ for ldx = 1:length(lambda)
             end
         end
 
-        
+        blg = sum(trans_list(:,1) > 0 && trans_list(:,1) == tdx) > 0;
+        if blg > 0
+            prob_ = rand(blg,1) <= prob;
+            trans_idx = trans_list(:,1) > 0;
+            trans_ = trans_list(trans_idx,:) .* prob_;
+            if sum(trans_(:,1) > 0) == 0
+                mu = max(mu - 1, 0);
+                lambda_recur = 0.995 * lambda_recur;
+            elseif sum(trans_(:,1) > 0) == 1
+                ndx_ = find(trans_list(:,2:4) == trans_(prob,2:4));
+                scs = scs + 1;
+                dly = dly + trans_(prob_,1) - trans_(prob_,2) + 1;
+                trans_list(ndx_,3) = trans_list(ndx_,3) - 1;
+                mu = max(mu - 1, 0);
+                lambda_recur = 0.995 * lambda_recur + 0.005;
+                if trans_list(ndx_,3) > 0
+                    trans_list(ndx_,1:2) = queue(ndx_,2);
+                    queue(ndx_,2:end) = queue(ndx_,1:end-1);
+                    trans_list(ndx_,4) = trans_list(ndx_,4) + 1; % synchronized
+                    % allocate slot to transmit
+                    bof = ceil((2 ^ trans_list(ndx_,3)) * mu * rand);
+                    while sum(bof_list == trans_list(ndx_,1) + bof) > 0
+                        bof = ceil((2 ^ trans_list(ndx_,3)) * mu * rand);
+                    end
+                    trans_list(ndx_,1) = trans_list(ndx_,1) + bof;
+                    bof_list(ndx_,1) = trans_list(ndx_,1) + bof;
+                end
+            else
+                find(trans_(:,4) > 0 && trans_(:,1) == tdx)
+            end
+        end
     end
 end
 toc
